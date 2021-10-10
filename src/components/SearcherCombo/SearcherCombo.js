2cf9c8759d77204533af622c2f5b5a8a05665912
adominguez <dmartin.alberto@gmail.com>
2021-10-10 11:46:30 +0200

    fix: remove unnecessary dependency

--- src/components/SearcherCombo/SearcherCombo.js
+++ src/components/SearcherCombo/SearcherCombo.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { getImage } from "gatsby-plugin-image"
 import { Wrapper, StyledImg } from './SearcherCombo.styles'
